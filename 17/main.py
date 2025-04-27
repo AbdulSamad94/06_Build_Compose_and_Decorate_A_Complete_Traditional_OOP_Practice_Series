11e442ba0d447c6db5a46341b604976b3577c391
AbdulSamad94 <abdulsamadsiddiqui2000@gmail.com>
2025-04-28 01:34:14 +0500

    17 done

---
+++ 17/main.py
@@ -0,0 +1,16 @@
+def add_greeting(cls):
+    def greet(self):
+        return "Hello from Decorator!"
+
+    cls.greet = greet
+    return cls
+
+
+@add_greeting
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+
+p = Person("Samad")
+print(p.greet())
